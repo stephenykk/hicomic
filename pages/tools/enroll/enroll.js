cbe01b03735fa192be7adf10e00f154f9cf82408
pan <zhenjie.pan@watsons.com.cn>
2019-02-15 12:51:23 +0800

    fix: title content may more then one line

--- pages/tools/enroll/enroll.js
+++ pages/tools/enroll/enroll.js
@@ -2,7 +2,33 @@ import sice from '../../../utils/sice';
 
 Page({
   data: {
-    names: '',
+    names: `搭食早餐、午餐的请接龙
+姓名:张三(早餐或午餐)
+1.吴炫霖   午餐
+2.龙梓茵丶午餐
+3.陈馨平、早餐，午餐
+4.赵岳瑜，午餐
+5.陈梓言，早餐，午餐
+6.程钊毅，早餐，午餐
+7.肖丹婷 午餐
+8、冯健钊，午餐
+9、吴正扬、午餐
+10、杜雨峻,早餐丶午餐。      11、邓灵 早餐，午餐
+12、王辰初   午餐
+13、刘梓萱、午餐 
+14、王子炎   午餐 
+15、苏茵婷   早餐   午餐
+16、李涛，午餐
+17.梁博瀚，午餐
+18.郭晓澄，早餐，午餐
+19.黎锦芬，午餐
+20.陈嘉莉，早餐，午餐
+21.刘东，午餐
+22.韩颖思，早餐，午餐
+23.黎焕彬，早餐，午餐                     24.代嘉欣，午餐                                   25.周予馨 ，午餐
+26.宗昊轩，午餐
+27.范佳麒，午餐
+28.舒乐，午餐                                     29.邓国浩，早餐，午餐`,
     sn: 0,
     newname: ''
   },
@@ -14,7 +40,7 @@ Page({
     
     // >=五个空格+序号姓名 则空格转换为换行
     let nlRe = /\s{5,}(\d)/g;
-    if(title && nlRe.test(title)) {
+    if(title && nlRe.test(title)) {// 如第1行包含有连续空格+序号的情况
         let arr = title.replace(nlRe, '\n$1').split('\n');
         title = arr.shift().trim();
         names = arr.concat(names);
@@ -30,13 +56,16 @@ Page({
 
     names = sice.flatten(names);
 
+    let firstNameIndex = names.findIndex(name => snLineRe.test(name)); // 之前的为活动文本
+    let notNameCount = firstNameIndex;
+
     // 规范化序号
     let enRe = /^[\d\s\.。、]+/;
     names = names.map((name, i) => {
-        return name.replace(enRe, `${sice.d2(i+1)}. `);
+        return i >= firstNameIndex ? name.replace(enRe, `${sice.d2(i+1-notNameCount)}. `) : name
     });
 
-    this.setData({sn: names.length});
+    this.setData({sn: names.length - notNameCount});
 
     if(title) {
         names.unshift(title);
@@ -83,6 +112,10 @@ Page({
     this.setData({newname: value});
   },
   onLoad: function () {
-    console.warn('enroll page load ---------->')
+    console.warn('enroll page load ---------->');
+    let {names} = this.data;
+    if(names) {
+        this.setData({names: this.handleRawNames(names)});
+    }
   }
 })
