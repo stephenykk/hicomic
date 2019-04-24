f0b9b6b9c1e99fbe54d735002051810f3bf134a3
stephenykk <stephenykk@163.com>
2019-04-25 02:21:05 +0800

    save home..

--- pages/paper/parsePaper.js
+++ pages/paper/parsePaper.js
@@ -5,28 +5,95 @@ var dataPath = path.resolve(__dirname, './data.txt');
 var outDataPath = path.resolve(__dirname, './data.js');
 
 function output(data) {
-	fs.writeFileSync(outDataPath, data, 'utf8');
+	if(typeof data === 'object') {
+		data = JSON.stringify(data, null, 2)
+	}
+
+	fs.writeFileSync(outDataPath, `module.exports = ${data}`, 'utf8');
+}
+
+function getQuestions() {
+	var con = fs.readFileSync(dataPath, 'utf8');
+	var lines = con.split(/\n/);
+	lines = lines.filter(line => {
+		line = line.replace(/^\s*|\s*$/, '');
+		return !!line;
+	})
+
+	var questions = [];
+	var titleRe = /^(\d+\s*\.[\s\S]*?)$/mg;
+	var lastTitleIdx = null;
+	lines.forEach((line, i) => {
+		var last = i === lines.length - 1;
+		if(last)  {
+			i = i + 1;
+		}
+
+		if(titleRe.test(line)) {
+			if(!lastTitleIdx) {
+				lastTitleIdx = i;
+			} else {
+				questions.push({title: lines[lastTitleIdx], options: lines.slice(lastTitleIdx + 1, i).join('\n')});
+				lastTitleIdx = i;
+			}
+		}
+	})
+
+	output(questions)
+	// console.log(rcon,'<0000000000')
+}
+
+
+function parseQuestions() {
+	var questions = require('./data.js');
+	questions.forEach(question => {
+		question.options = parseOptions(question);
+	})
+
+	output(questions);
+}
+
+function parseOptions(question) {
+	if(Array.isArray(question.options)) {
+		return;
+	}
+	if(question.options == null) {
+		console.log('NO OPTIONS, 应该是判断题');
+		console.log('question: ', question);
+		return;
+	}
+
+	if(!(question.options && question.options.split)) {
+		console.log('BAD OPTIONS');
+		console.log('question: ', question);
+		throw new Error('stop and check it..')
+	}
+
+	var options = question.options.split(/\n|\r/);
+	options = options.filter(opt => {
+		opt = opt.replace(/^\s*|\s*$/g, '');
+		return !!opt;
+	})
+
+	if(options.length === 1) {
+		options = options[0].match(/[A-G]\s*?\.(.(?![A-G]\s*?\.))*./g)
+		options.map(opt => opt.trim());
+	}
+
+	console.log('[INFO] parse options: ', question.options , '-->', options);
+
+	return options;
+}
+
+
+
+function parseTitle(question) {
+	
 }
 
 function main() {
-	var con = fs.readFileSync(dataPath, 'utf8');
-	var questionRe = /^(\d+\s*\.[\s\S]*?)$^(?!\d+)\n?([\s\S]*?)/mg;
-	var questions = [];
-	var i = 0;
-	var rcon = con.replace(questionRe, (match, title, options) => {
-		console.log('[MSG] match:', match);
-		console.log('[MSG] title:', title);
-		console.log('[MSG] options:', options);
-		i++;
-		if(i > 5) {
-			throw new Error('stop and see')
-		}
-		// questions.push({title, options});
-		return '';
-	});
-
-	// output(JSON.stringify(questions, null, 2))
-	// console.log(rcon,'<0000000000')
+	// getQuestions();
+	parseQuestions();
 }
 
 main();
